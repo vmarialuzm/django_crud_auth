f47a8b7cd25ca07f9341eb0900f72f3e2bf20cd3
vmarialuz <vmarialuzm@gmail.com>
2023-09-28 12:26:55 -0500

    Formulario de tareas

--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+from .forms import TaskForm
 
 # Create your views here.
 def home(request):
@@ -34,6 +35,16 @@ def signup(request):
 def tasks(request):
     return render(request, 'tasks.html')
 
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+            'form': TaskForm
+        })
+    else:
+        print(request.POST)
+        return render(request, 'create_task.html', {
+            'form': TaskForm
+        })
 def signout(request):
     logout(request)
     return redirect('home')
