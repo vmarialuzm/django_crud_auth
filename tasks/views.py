30db6c220ae985cc7aa4a6d61e1bb50eb5a08196
vmarialuz <vmarialuzm@gmail.com>
2023-09-29 10:47:12 -0500

    listar tareas

--- tasks/views.py
+++ tasks/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
+from .models import Task
 
 # Create your views here.
 def home(request):
@@ -33,7 +34,8 @@ def signup(request):
         })
     
 def tasks(request):
-    return render(request, 'tasks.html')
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'tasks.html', {'tasks': tasks})
 
 def create_task(request):
     if request.method == 'GET':
